8b35a8ea4e785b15257803e14978967b2fa6509a
Maharani Nirmala Dewi <maharanisakti015@Gmail.com>
2022-07-29 17:23:30 +0700

    update repository class

--- app/src/main/java/com/example/photoalbum/MyImagesRepository.java
+++ app/src/main/java/com/example/photoalbum/MyImagesRepository.java
@@ -49,7 +49,7 @@ public class MyImagesRepository {
         });
     }
 
-    public LiveData<List<MyImages>> getImageList() {
+    public LiveData<List<MyImages>> getAllImages() {
         return imageList;
     }
 
