8133d5fc445b975d7abc059272569c746b14bc13
Maharani Nirmala Dewi <maharanisakti015@Gmail.com>
2022-08-04 18:06:04 +0700

    selecting image

--- app/src/main/java/com/example/photoalbum/MainActivity.java
+++ app/src/main/java/com/example/photoalbum/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.photoalbum;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,19 +24,25 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton fab;
     private MyImagesViewModel myImagesViewModel;
 
+    private ActivityResultLauncher<Intent>activityResultLauncherForAddImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//      register activity
+        registerActivityForAddImage();
+        rv=findViewById(R.id.rv);
+        fab=findViewById(R.id.fab);
+
         rv.setLayoutManager(new LinearLayoutManager(this));
 
         MyImagesAdapter adapter= new MyImagesAdapter();
         rv.setAdapter(adapter);
 
-        rv=findViewById(R.id.rv);
-        fab=findViewById(R.id.fab);
+
 
         myImagesViewModel= new ViewModelProvider
                 .AndroidViewModelFactory(getApplication())
@@ -48,7 +59,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                Intent intent=new Intent(MainActivity.this,AddImageActivity.class);
+//                activityResultLauncher
+                activityResultLauncherForAddImage.launch(intent);
             }
         });
+    }
+    public void registerActivityForAddImage(){
+
+        activityResultLauncherForAddImage=registerForActivityResult(new ActivityResultContracts
+                .StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+
+            }
+        });
+
     }
 }
